ecfafaef71c548a79d15feeca8bfd4c80162c37e
annewaithaka <annewaithaka68@gmail.com>
2024-09-09 12:29:30 +0300

    done

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,26 @@
-#!/usr/bin/env python3
+# book.py
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self.title = title
+        self._page_count = page_count  # Use a private attribute to manage page_count
+        self.validate_page_count()
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+            return
+        self._page_count = value
+
+    def validate_page_count(self):
+        if not isinstance(self._page_count, int):
+            raise ValueError("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,26 @@
-#!/usr/bin/env python3
+# shoe.py
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = size
+        self.validate_size()
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+            return
+        self._size = value
+
+    def validate_size(self):
+        if not isinstance(self._size, int):
+            raise ValueError("size must be an integer")
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
